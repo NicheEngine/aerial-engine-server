69bd25aaa99aa466b1c5981c5ddddd5b3aae4847
Cyan <snow22314@outlook.com>
2025-07-18 18:18:01 +0800

    删除日志

--- aerial-engine-server/src/main/java/io/github/nicheengine/aerial/controller/AerialHelloController.java
+++ aerial-engine-server/src/main/java/io/github/nicheengine/aerial/controller/AerialHelloController.java
@@ -7,10 +7,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * <code>AerialHelloController</code>
+ * <p>The aerial hello controller class.</p>
+ * @author Cyan (snow22314@outlook.com)
+ * @see org.springframework.web.bind.annotation.RestController
+ * @see org.springframework.web.bind.annotation.RequestMapping
+ * @since Jdk1.8
+ */
 @RestController
 @RequestMapping("/aerial/v1.0.0")
 public class AerialHelloController {
 
+    /**
+     * <code>hello</code>
+     * <p>The hello method.</p>
+     * @return {@link io.github.nichetoolkit.rest.RestResult} <p>The hello return object is <code>RestResult</code> type.</p>
+     * @see io.github.nichetoolkit.rest.RestResult
+     * @see io.github.nichetoolkit.rice.stereotype.RestSkip
+     * @see org.springframework.web.bind.annotation.RequestMapping
+     */
     @RestSkip
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public RestResult<?> hello() {
