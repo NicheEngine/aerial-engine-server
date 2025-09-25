94a425265cf453b8a182e42e935accf9be6f00af
Cyan <snow22314@outlook.com>
2025-09-25 11:51:39 +0800

    fix

--- aerial-engine-server/src/main/java/io/github/nicheengine/aerial/controller/AerialLoginController.java
+++ aerial-engine-server/src/main/java/io/github/nicheengine/aerial/controller/AerialLoginController.java
@@ -41,9 +41,16 @@ public class AerialLoginController {
 
     @RestLogin
     @PostMapping("/token")
-    @RestUserlog(loggingType = LoggingType.USER_LOGIN, userlog = "访问令牌登录")
-    public RestResult<UserModel> loginWithAccessToken(TokenContext context, @RequestBody LoginBody loginBody) throws RestException {
+    @RestUserlog(loggingType = LoggingType.USER_LOGIN, userlog = "令牌登录")
+    public RestResult<UserModel> loginWithToken(TokenContext context, @RequestBody LoginBody loginBody) throws RestException {
         UserModel user = loginService.loginWithToken(loginBody);
+        return buildLoginResult(context, user);
+    }
+
+    @RestLogin
+    @PostMapping("/refresh")
+    @RestUserlog(loggingType = LoggingType.USER_LOGIN, userlog = "令牌刷新")
+    public RestResult<UserModel> loginWithRefresh(TokenContext context, @RestUser UserModel user) throws RestException {
         return buildLoginResult(context, user);
     }
 
