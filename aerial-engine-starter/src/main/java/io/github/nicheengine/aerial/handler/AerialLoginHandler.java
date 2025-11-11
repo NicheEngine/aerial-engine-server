3bdd1ec296003514a5968f253606db849c27f4c4
Cyan <snow22314@outlook.com>
2025-11-11 17:37:26 +0800

    fix

--- aerial-engine-starter/src/main/java/io/github/nicheengine/aerial/handler/AerialLoginHandler.java
+++ aerial-engine-starter/src/main/java/io/github/nicheengine/aerial/handler/AerialLoginHandler.java
@@ -75,7 +75,7 @@ public class AerialLoginHandler implements LoginAdvice {
     }
 
     @Override
-    public void doLogoutHandle(RestHttpRequest request, Object body, MethodParameter returnType, TokenContext context) throws RestException {
+    public void doLogoutHandle(RestHttpRequest request) throws RestException {
         UserModel userModel;
         try {
             userModel = tokenService.resolveUserInfo(request);
